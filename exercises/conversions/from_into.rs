3d8cc646f2d92e61b6f9c9aa44ea1fdd464bd473
sunjiayu <767978963@qq.com>
2024-10-13 22:43:21 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let tmp = s.split(',').map(|x| x.to_string()).collect::<Vec<String>>();
+        if tmp.len() != 2 {
+            return Person::default();
+        }
+        if tmp[0].len() == 0 {
+            return Person::default();
+        }
+        match tmp[1].parse() {
+            Ok(age) => Person {
+                name: tmp[0].clone(),
+                age: age,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
